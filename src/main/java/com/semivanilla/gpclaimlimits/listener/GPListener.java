11647ceb5d35a8ab9742e914bef6f4f8b0b166da
Len <40720638+destro174@users.noreply.github.com>
2023-04-01 21:56:19 +0200

    Do not limit claims if it is a subclaim.

--- src/main/java/com/semivanilla/gpclaimlimits/listener/GPListener.java
+++ src/main/java/com/semivanilla/gpclaimlimits/listener/GPListener.java
@@ -3,7 +3,6 @@ package com.semivanilla.gpclaimlimits.listener;
 import com.semivanilla.gpclaimlimits.config.Config;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import me.ryanhamshire.GriefPrevention.events.ClaimCreatedEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,6 +16,8 @@ public class GPListener implements Listener {
         if (!(event.getCreator() instanceof Player player)) return;
 
         if (player.hasPermission("semivanilla.claimlimit.bypass")) return;
+
+        if (event.getClaim().parent != null) return;
 
         int numberOfClaimsAfterEvent = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId()).getClaims().size() + 1;
         int claimLimitFromPermission = -1;
